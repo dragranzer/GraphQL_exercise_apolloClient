58f3150b98b4d1f4151785de8eb392fbe4478a64
Ivan Muhammad Nizar <ivannizar@gmail.com>
2021-09-21 09:14:49 +0700

    first commit

---
+++ src/apollo-client.js
@@ -0,0 +1,14 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'https://eminent-treefrog-39.hasura.app/v1/graphql',
+    cache: new InMemoryCache(),
+    headers: {
+        "x-hasura-admin-secret" : "oGzx4jqbIbRAIVGfB5W1FgkXmWkwWJrWwuQx1qeFQTQH13uGkTCNxDEpXHydS3pk"
+    }
+});
+
+export default client;
